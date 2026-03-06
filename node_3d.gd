637f8eee80561a67a52bb96b211907e3cb6933cd
Mutedfish <mutedfishman@gmail.com>
2026-03-07 00:10:18 +0200

    gameplay to menu connection
    - now day 1 button sends you to the gameplay test
    - now you cant play the same animation multiple times
    - fixed the main menu animation
    (probobly a fix? vasco explain to me later?)

--- node_3d.gd
+++ node_3d.gd
@@ -1,8 +1,19 @@
 extends Node3D
 
+#so pressing the same key multiple time would'nt play the same animation
+#probobly change event to something else (IDK)
+enum events{
+	desk,
+	customers
+}
+
+var last_event = null
 
 func _input(event: InputEvent) -> void:
-	if event.is_action("S"):
+	if event.is_action("S") and last_event != events.customers:
 		$AnimationPlayer.play("desk_to_customers")
-	elif event.is_action("A"):
+		last_event = events.customers
+
+	elif event.is_action("A") and last_event != events.desk:
 		$AnimationPlayer.play("customers_to_desk")
+		last_event = events.desk
