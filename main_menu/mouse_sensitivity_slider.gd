3ad973ce2438d1e323fb0a93affeac7f5d047f03
Mutedfish <mutedfishman@gmail.com>
2026-03-07 02:58:00 +0200

    menu background

--- main_menu/mouse_sensitivity_slider.gd
+++
@@ -1,4 +0,0 @@
-extends HSlider
-
-func drag_ended() -> void:
-	get_tree().mouse_sensetivity = tick_count 
--- main_menu/settings_apply_button.gd
+++
@@ -1,8 +0,0 @@
-extends GenericButtonSuper
-
-
-func _pressed() -> void:
-	#Audio
-	AudioServer.set_bus_volume_db(0, linear_to_db($"../AudioSetting/MasterSoundSlider".value))
-	AudioServer.set_bus_volume_db(1, linear_to_db($"../AudioSetting/MusicSlider".value))
-	AudioServer.set_bus_volume_db(2, linear_to_db($"../AudioSetting/SFXSlider".value))
