23a842b9d77ad7589c9ce9cd0c6f96762ab43080
Cake <gamerbolin0101@gmail.com>
2026-03-18 18:41:06 -0300

    Temporary patch for play button
    The play button simply crashed the game when pressing it. I made a small patch, but a proper fix should be made later, I hope

--- scripts/main_menu_scripts/menu_play_button.gd
+++ scripts/main_menu_scripts/menu_play_button.gd
@@ -7,7 +7,9 @@ func _ready() -> void:
 
 func _pressed() -> void:
 	anim_player.play("fade in")
-	$ButtonClicked.play()
+	# (Cake) I have no idea on what ButtonClicked is, but it doesn't seem to
+	# exist, so it simply just crashes the game. (´。＿。｀)
+	#$ButtonClicked.play()
 
 func _on_transition_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "fade in":
