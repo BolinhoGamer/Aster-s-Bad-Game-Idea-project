8c3adada3b0667691a0db4bb2d102abfbc33447e
Mutedfish <mutedfishman@gmail.com>
2026-03-18 21:29:54 +0200

    bug fix
    - you are no longer trapped in the credits/settings menu

--- scripts/main_menu_scripts/menu_return_button.gd
+++ scripts/main_menu_scripts/menu_return_button.gd
@@ -1,15 +1,6 @@
 extends GenericButtonSuper
 
+@onready var main_node := $".."
 
-var was_pressed := false
 func _pressed() -> void:
-	if was_pressed == false:
-		was_pressed = true
-	#a second press means cancel listening
-	else:
-		was_pressed = false
-
-#(mutedfish)to much work for me rn, will do later
-#func _input(event: InputEvent) -> void:
-#	if event is InputEventKey and event.is_pressed() and is_valid_key(event):
-#		InputHelper.
+	main_node.state = main_node.eGlobalGameState.e_ggs_on_main_menu
